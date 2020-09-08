086575fb932cb68f6076e256c8ac38e43a57c55c
sandrotoline <sandro.toline@gmail.com>
2020-09-08 00:47:40 +0000

    Update readme.md

--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -1,13 +1,34 @@
 package br.gov.sp.fatec.springbootapp;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.gov.sp.fatec.springbootapp.entity.Usuario;
+import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
 
 @SpringBootTest
+@Transactional
+@Rollback
 class SpringBootAppApplicationTests {
+
+    @Autowired
+    private UsuarioRepository usuarioRepo;
 
 	@Test
 	void contextLoads() {
 	}
+    @Test
+    void testInsercao (){
+        Usuario usuario = new Usuario();
+        usuario.setNome("teste");
+        usuario.setSenha("124Teste");
+        usuarioRepo.save(usuario);
+
+        assertNotNull(usuario.getId());
+    }
 
 }
